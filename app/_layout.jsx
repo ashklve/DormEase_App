322036d053cc6f66fd3fb7483b47f1418869d2da
9rzaaa <lingdaniel999@gmail.com>
2026-06-22 18:03:11 +0800

    feat(emergency): add quick emergency trigger

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import { UserProvider } from '../src/context/UserContext';
 import PushNotificationBootstrap from '../src/components/PushNotificationBootstrap';
+import QuickEmergencyTrigger from '../src/components/QuickEmergencyTrigger';
 import LoadingOverlay from '../src/components/LoadingOverlay';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react';
@@ -25,6 +26,7 @@ export default function RootLayout() {
     return (
         <UserProvider>
             <PushNotificationBootstrap />
+            <QuickEmergencyTrigger />
             <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="index" />
                 <Stack.Screen name="welcome" />
